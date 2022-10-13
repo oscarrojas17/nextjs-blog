c0bdd152b40b662bb5cff42373a113bd2679a671
Oscar Rojas <61994689+oscarrojas17@users.noreply.github.com>
2022-10-13 23:57:55 +0000

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,46 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello, I'm Oscar. I was the founder and CEO of a YC-backed startup where I learned fundraising, sales, and product. I like building things so I decided to learn to design and code. Now focusing on the building part for a while.</p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+  <Link href={`/posts/${id}`}>
+    <a>{title}</a>
+  </Link>
+  <br />
+  <small className={utilStyles.lightText}>
+    <Date dateString={date} />
+  </small>
+</li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
