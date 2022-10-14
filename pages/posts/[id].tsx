6ab0248abbd4a292ca75ceea193e126f994dc659
Oscar Rojas <61994689+oscarrojas17@users.noreply.github.com>
2022-10-14 20:14:22 +0000

    updated code in hello.ts

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -39,7 +39,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const postData = await getPostData(param1s.id as string)
+  const postData = await getPostData(params.id as string)
   return {
     props: {
       postData
